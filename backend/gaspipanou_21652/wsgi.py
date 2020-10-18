5d8e723d0fef1cca172f2a63596ab4527f8a0494
Crowdbotics <team@crowdbotics.com>
2020-10-18 07:53:55 +0000

    Update models

--- backend/gaspipanou_21652/wsgi.py
+++ backend/gaspipanou_21652/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gaspipanou_21652.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gaspipanou_21652.settings")
 
 application = get_wsgi_application()
